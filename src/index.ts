869aa4cd1be810f86aeff51db7498033d1fe9c80
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-08-13 20:06:33 -0300

    project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response }  from "express";
+import { createAudioStreamFromText } from "./services/elevenLabs/text_to_speech_file";
+import cors from "cors"
+import { constrainedMemory } from "process";
+
+const app = express()
+app.use(cors())
+const PORT = process.env.PORT || 3000
+
+app.use('/text-to-speech/:text', async (req: Request, res: Response) => {
+    try{
+        const text: any = req.params.text
+        const audioBuffer  = await createAudioStreamFromText(text)
+        res.setHeader("Content-Type", "audio/mpeg");
+        return res.send(audioBuffer);
+    }catch(err){
+        console.log('erro nesse carai, ' + err)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
